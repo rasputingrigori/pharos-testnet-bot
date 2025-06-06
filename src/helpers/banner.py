06c59496c2bce0fd29f2df4bfd96bde48440bfd6
rasputingrigori <rinatanolan2@gmail.com>
2025-06-06 22:21:37 +0700

    initial commit

--- src/helpers/banner.py
+++ src/helpers/banner.py
@@ -10,7 +10,7 @@ def s0x000():
       |_|\_||_(___/|_|     \____|\___)_|_| |_|
                                               
 """ 
-    print(Fore.WHITE + f" Huddle01 Testnet -- [MEETING] Automations")
-    print(Fore.WHITE + f" Github {Fore.LIGHTGREEN_EX}",Style.RESET_ALL)
+    print(Fore.WHITE + f" Pharos Testnet -- Full Automations")
+    print(Fore.WHITE + f" Github {Fore.LIGHTGREEN_EX}github.com/rasputingrigori/pharos-testnet-bot",Style.RESET_ALL)
     print(Fore.LIGHTWHITE_EX + Style.BRIGHT + s0x001 + Style.RESET_ALL)
     print(f"-" * 69)
